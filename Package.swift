5926cb27fa367ffb6e1450fac13af9c4dc814a48
Dave Poirier <dave@madefresh.ca>
2017-06-19 09:28:00 -0400

    Updated to be compatible with latest Swift Package manager
    Signed-off-by: Dave Poirier <dave@madefresh.ca>

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Bitter",
-    targets: [
-        Target(name: "BitterTests", dependencies: [.Target(name: "Bitter")])
-    ]
+	targets: [
+		Target(name: "Bitter", dependencies: [])
+	]
 )
